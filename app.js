5bd479649a4368762f94929eeefaa829cccc9835
zumer305 <zumerniaz305@gmail.com>
2025-10-07 16:46:17 +0500

    push

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 const express=require("express");
 const app=express();
 
-// middleware 
-app.use((req,res,next)=>{
-    // let{query}=req.query;
-    // console.log(query);
-    console.log("Hi I am  first middleware");
-    // res.send("midleware finished");
-    next();
+// // middleware 
+// app.use((req,res,next)=>{
+//     // let{query}=req.query;
+//     // console.log(query);
+//     console.log("Hi I am  first middleware");
+//     // res.send("midleware finished");
+//     next();
 
-})
-app.use((req,res,next)=>{
-    console.log("Hi I am 2nd middleware");
-    next();
+// })
+// app.use((req,res,next)=>{
+//     console.log("Hi I am 2nd middleware");
+//     next();
 
+// })
+
+
+
+//LOGGER MORGAN
+app.use((req,res,next)=>{
+    req.time=Date.now();
+    console.log(req.method,req.hostname,req.path,req.time);
+    next();
 })
+
+
+
+
 
 
 app.get("/",(req,res)=>{
